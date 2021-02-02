26d88a0066c77da3cdd00f4f4c7bc0902d0661e1
faturay <fatur_207@smkassalaambandung.sch.id>
2021-02-02 05:51:55 -0400

    blm beres

--- lara-vue-spa/routes/api.php
+++ lara-vue-spa/routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Models\Provinsi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProvinsiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +17,3 @@ use App\Http\Controllers\Api\ProvinsiController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-// API Provinsi
-Route::get('provinsi', [ProvinsiController::class, 'index']);
-Route::post('provinsi', [ProvinsiController::class, 'store']);
-Route::get('provinsi/{id}', [ProvinsiController::class, 'show']);
-Route::post('provinsi/update/{id}', [ProvinsiController::class, 'update']);
-Route::delete('/provinsi/{id}', [ProvinsiController::class, 'destroy']);
--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -27,7 +27,9 @@
                 <a href="./index.html" class="nav-link active">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Provinsi</p>
-                </a>
+                  </a>
+                 </li>
+               </ui>
               </li>
               <li class="nav-item">
                 <a href="./index2.html" class="nav-link">
